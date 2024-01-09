b055278a51b6c8485d301eed2e231ab281731e03
Lovepreet Zelenkov Singh <113479651+Lpreets@users.noreply.github.com>
2024-01-09 16:17:16 +0100

    Authentication added with Clerk and Dark Mode with Shadcn

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import { Poppins }from "next/font/google"
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const Logo = () => {
+    return (
+        <div className="flex flex-col items-center gap-y-4">
+            <div className="bg-white rounded-full p-1">
+            <Image 
+                src="/spooky.svg"
+                alt="logo"
+                width={80}
+                height={80}
+            />
+            </div>
+            <div className={cn("flex flex-col items-center", font.className)}>
+                <p className="text-xl font-semibold">
+                    Twitch Clone
+                </p>
+                <p className="text-sm text-muted-foreground">
+                    Let&apos;s play
+                </p>
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <p className="text-red-500 font-bold">
-      Hello Twitch Clone
-    </p>
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
